188e90d6eb2b73f78c405ef3f3203edf10a17c94
hyun <jh9501@gmail.com>
2023-10-29 01:22:08 +0900

    REFACTOR: Replace viewModel

--- WeatherFit/ContentView.swift
+++ WeatherFit/ContentView.swift
@@ -11,72 +11,48 @@ import CoreLocation
 
 struct ContentView: View {
     @ObservedObject var viewModel = WeatherFitManager()
+    @State private var scrollViewSize: CGSize = .zero
 //    @State var location = ""
     
     var body: some View {
         VStack {
             Spacer()
             VStack{
-                VStack{
-//                    Text(viewModel.userAddress())
-//                        .font(.largeTitle)
-//                        .padding()
-                    Text("현재 기온")
-                    Text(("\(viewModel.currTemp)"))
-                        .font(.largeTitle)
-                    Image(systemName: viewModel.symbol)
-                    VStack{
-                        Text("외출시 평균 기온")
-                        Text("\(viewModel.startTime) ~ \(viewModel.endTime)")
-                        Text("\(viewModel.avgTemp)")
-                    }
-                    .padding(.vertical)
-                    
-//                    Button(action: {
-//                        viewModel.changeTimeRange()
-//                        UserDefaults.standard.setValue(viewModel.startTime, forKey: "StartTime")
-//                        UserDefaults.standard.setValue(viewModel.endTime, forKey: "EndTime")
-//                    }, label: {
-//                        Text("변경")
-//                    })
-                }
-                
-                ScrollView(.horizontal, showsIndicators: false){
-                    LazyHStack(alignment: .top, spacing: 24, content: {
-                        ForEach(viewModel.hourlyWeatherInfo) { info in
-                            WeatherView(date: info.date, condition: info.condition, temp: info.temp, symbol: info.symbolName)
-                        }
-                    })
-                    .fixedSize()
-                }
-                .padding(.all)
-                
-//                Text(viewModel.recommendFit)
+                Text("현재 기온")
+                Text(("\(viewModel.currTemp)"))
+                    .font(.largeTitle)
+                Image(systemName: viewModel.currSymbolName)
+                    .font(.title)
             }
             
+            VStack{
+                Text("외출시 평균 기온")
+                Text("\(viewModel.startTime) ~ \(viewModel.endTime)")
+                Text("\(viewModel.avgTemp)")
+            }
+            .padding(.vertical)
+            
+            ScrollView(.horizontal, showsIndicators: true){
+                LazyHStack(alignment: .center, spacing: 24, content: {
+                    ForEach(viewModel.hourlyWeatherInfo) { info in
+                        WeatherView(date: info.date, condition: info.condition, temp: info.temp, symbol: info.symbolName)
+                    }
+                })
+            }
+            .padding(.horizontal)
+            .frame(height: 70)
             
             Spacer()
-            VStack{
-                HStack{
-                    timePicker(selectedTime: $viewModel.startTime)
-                    timePicker(selectedTime: $viewModel.endTime)
-                }
+            Text(viewModel.recommendFit)
+            Spacer()
+            HStack{
+                timePicker(selectedTime: $viewModel.startTime)
+                timePicker(selectedTime: $viewModel.endTime)
             }
         }
         .task{
             await viewModel.getWeather()
         }
-//        .onAppear(){
-//            viewModel.startTime = UserDefaults.standard.string(forKey: "StartTime") ?? "07:00"
-//            viewModel.endTime = UserDefaults.standard.string(forKey: "EndTime") ?? "09:00"
-//            print(viewModel.startTime)
-//            print(viewModel.endTime)
-//            print(viewModel.timeRange())
-//        }
-//        .onDisappear(){
-//            UserDefaults.standard.setValue(viewModel.startTime, forKey: "StartTime")
-//            UserDefaults.standard.setValue(viewModel.endTime, forKey: "EndTime")
-//        }
     }
 }
 
@@ -90,6 +66,7 @@ struct WeatherView: View{
         VStack{
             Text("\(Calendar.current.component(.hour, from: date))")
             Image(systemName: symbol)
+                .font(.title)
             Text("\(Int(temp.value.rounded()))")
         }
     }
@@ -106,15 +83,15 @@ struct ClosetView: View{
 
 struct timePicker: View{
     @Binding var selectedTime:Int
-    let times = [Int](0...23)
-//    let times = ["0:00","1:00","2:00","3:00","4:00","5:00","6:00","7:00","8:00","9:00","10:00","11:00","12:00",
-//                 "13:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00","22:00","23:00"]
+    let idx = [Int](0...23)
+    let times = ["0:00","1:00","2:00","3:00","4:00","5:00","6:00","7:00","8:00","9:00","10:00","11:00","12:00",
+                 "13:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00","22:00","23:00"]
     
     var body: some View{
         VStack{
             Picker("time picker", selection: $selectedTime, content:{
-                ForEach(times, id:\.self){ time in
-                    Text("\(time)")
+                ForEach(idx, id:\.self){ idx in
+                    Text(times[idx])
                 }
             })
             .pickerStyle(.wheel)
