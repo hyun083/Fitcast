26cbd198fd9ae486af603ad3b4b03d9f13d824f3
hyun <jh9501@gmail.com>
2023-11-22 00:25:53 +0900

    STYLE: Change timePicker property declare syntax
    for dynamic time range indexing

--- WeatherFit/ContentView.swift
+++ WeatherFit/ContentView.swift
@@ -99,8 +99,8 @@ struct ContentView: View {
                 
                 //MARK: - timePicker
                 HStack{
-                    timePicker(selectedTime: $viewModel.startTime)
-                    timePicker(selectedTime: $viewModel.endTime)
+                    timePicker(selectedTime: $viewModel.startTime, idx: [Int](0...23))
+                    timePicker(selectedTime: $viewModel.endTime, idx: [Int](0...23))
                 }
             }
             .task{
@@ -177,7 +177,7 @@ struct ClosetView: View{
 //MARK: - TimePickerView
 struct timePicker: View{
     @Binding var selectedTime:Int
-    let idx = [Int](0...23)
+    let idx: [Int]
     let times = ["0:00","1:00","2:00","3:00","4:00","5:00","6:00","7:00","8:00","9:00","10:00","11:00","12:00",
                  "13:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00","22:00","23:00"]
     
