beecdb383b7a6a62fa3403cd66d6765e057bf0b1
hyun <jh9501@gmail.com>
2024-05-26 21:54:40 +0900

    FIX: Fix updating location list on widget configuration edit

--- FitcastWidget/AppIntent.swift
+++ FitcastWidget/AppIntent.swift
@@ -11,13 +11,18 @@ import CoreLocation
 
 struct WidgetLocation: AppEntity{
     var id: String
-    var locatoin: CLLocation
+    var location: CLLocation
     
     static var typeDisplayRepresentation: TypeDisplayRepresentation = "Location"
     static var defaultQuery = WidgetLocationQuery()
-    static var locationList = getLocationList()
-    
-    private static func getLocationList() -> [FitcastLocation]{
+   
+    var displayRepresentation: DisplayRepresentation{
+        DisplayRepresentation(title: "\(id)")
+    }
+}
+
+struct WidgetLocationQuery: EntityQuery {
+    func getLocationList() -> [FitcastLocation]{
         if let data = UserDefaults.shared.data(forKey: "locationList"){
             return try! PropertyListDecoder().decode([FitcastLocation].self, from: data)
         }else{
@@ -25,29 +30,29 @@ struct WidgetLocation: AppEntity{
         }
     }
     
-    var displayRepresentation: DisplayRepresentation{
-        DisplayRepresentation(title: "\(id)")
-    }
-    
-    static let locations: [WidgetLocation] = [
-        WidgetLocation(id: "나의 위치", locatoin: CLLocation(latitude: 37.27807821976637, longitude: 127.15216520791188))
-    ] 
-//    + locationList.map{WidgetLocation(id: $0.locality, locatoin: CLLocation(latitude: $0.latitude, longitude: $0.longitude))}
-}
-
-struct WidgetLocationQuery: EntityQuery {
     func entities(for identifiers: [WidgetLocation.ID]) async throws -> [WidgetLocation] {
-        WidgetLocation.locations.filter{
-            identifiers.contains($0.id)
-        }
+        let locationList = getLocationList().map{WidgetLocation(id: $0.locality, location: CLLocation(latitude: $0.latitude, longitude: $0.longitude))}
+        
+        let locations: [WidgetLocation] = [
+            WidgetLocation(id: "나의 위치", location: CLLocation(latitude: 37.27807821976637, longitude: 127.15216520791188))
+        ] + locationList
+        
+        return locations
     }
     
     func suggestedEntities() async throws -> [WidgetLocation] {
-        WidgetLocation.locations
+        //widget configuration
+        let locationList = getLocationList().map{WidgetLocation(id: $0.locality, location: CLLocation(latitude: $0.latitude, longitude: $0.longitude))}
+        
+        let locations: [WidgetLocation] = [
+            WidgetLocation(id: "나의 위치", location: CLLocation(latitude: 37.27807821976637, longitude: 127.15216520791188))
+        ] + locationList
+        
+        return locations
     }
     
     func defaultResult() async -> WidgetLocation? {
-        WidgetLocation.locations.first
+        return WidgetLocation(id: "나의 위치", location: CLLocation(latitude: 37.27807821976637, longitude: 127.15216520791188))
     }
 }
 
